9a5f70d4ebdf4f9b064777561ed3bec1364873c2
Kshitij Kotecha <kshitijkotecha@Kshitijs-MacBook-Air.local>
2024-01-15 15:18:07 +0530

    liveCompleted

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -8,6 +8,31 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // complete the function below:
 function Carousel() {
+    const [currindex, setcurrIndex] = useState(0)
+
+    function handlePrev(){
+        setcurrIndex(currindex > 0 ? currindex -1 : images.length - 1)
+    }
+
+    function handleNext(){
+        setcurrIndex(currindex < images.length - 1 ? currindex + 1 : 0)
+    }
+    
+    return(
+        <>
+        <div className="carousel-countainer flex">
+            <div className="leftArrow arrowDiv flex" onClick= {handlePrev}>
+                <ArrowBackIosIcon/>
+            </div>
+            <div className="rightArrow arrowDiv flex" onClick= {handleNext}>
+                <ArrowForwardIosIcon/>
+            </div>
+            <h2 className="title" >{images[currindex].title}</h2>
+            <h4 className="caption"> {images[currindex].subtitle}</h4>
+            <img src= {images[currindex].img}/>
+        </div>
+        </>
+    )
 }
 
 export default Carousel;
